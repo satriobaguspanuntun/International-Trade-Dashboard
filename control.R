8c0c27ccb6dd4df699a39183099b21b65bf816c8
satriobaguspanuntun <satriobaguspanuntun@yahoo.com>
2025-02-27 15:51:05 +0700

    update

--- control.R
+++ control.R
@@ -150,13 +150,14 @@ loop_across_countries <- function(batches, start, end, hs) {
   # add id column
   # trade id
   final_trade_data <- final_trade_data %>% 
-    mutate(id = paste0(ref_period_id, reporter_iso, flow_code, cmd_code, partner_iso)) %>% 
+    mutate(id = paste0(ref_period_id, reporter_iso, flow_code, cmd_code, partner_iso),
+           id = if_else(!grepl("^[0-9]", id), NA, id)) %>% 
     relocate(id)
   
   # service id
   final_service_data <- final_service_data %>% 
     mutate(id = paste0(ref_period_id, reporter_iso, flow_code, cmd_code, partner_iso),
-           id = if_else(grepl("^[0-9]", id), NA, id)) %>% 
+           id = if_else(!grepl("^[0-9]", id), NA, id)) %>% 
     relocate(id)
   
   # macro id
@@ -175,11 +176,74 @@ loop_across_countries <- function(batches, start, end, hs) {
 }
 
 # create sqlite database and update or save data on the first instance
-sqlite_push <- function(data_list){
+sqlite_push <- function(data_list, overwrite = FALSE){
   
-  if (file.exists("~/international-Trade-Dashboard/master_db.sqlite")) {
+  # dataset names
+  dataset_names <- names(data_list)
+  
+  if (file.exists("~/international-Trade-Dashboard/master_db.db")) {
+    
+    for (i in dataset_names) {
+      
+      data <- data_list[[paste0(i)]]
+      
+      if (i %in% c("goods", "services")) {
+        
+        # construct sql queries
+        sql_query <- sprintf("SELECT DISTINCT ref_year, reporter_iso FROM %s", i)
+        sql_data_for_join <- dbGetQuery(conn, sql_query)
+        
+        # new data summarised
+        new_data_for_join <- data %>% group_by(ref_year, reporter_iso) %>%  summarise(n = n()) %>% select(-n)
+        
+        # anti join both dataframe (this will collect the remainder data rows that are not in the sql database)
+        new_data_filter <- anti_join(new_data_for_join, sql_data_for_join, by = join_by(ref_year, reporter_iso))
+        
+        if (nrow(new_data_filter) > 0) {
+          
+          # filter new data
+          new_data <- data %>% filter(ref_year %in% new_data_filter$ref_year, reporter_iso %in% new_data_filter$reporter_iso)
+          
+          # append new data into sqlite table
+          dbAppendTable(conn, name = i, value = new_data)
+          
+          
+        } else {
+          
+          cli::cli_h3(paste0("The new data dimesion and content are exactly matched with the one in the base"))
+          
+          overwrite_table <- rstudioapi::showQuestion(title = "Overwriting option", 
+                                                      message = "Would you like to overwrite the table?",
+                                                      ok = "Yes", cancel = "No")
+          if (overwrite_table) {
+            
+            dbWriteTable(conn, name = i, value = data)
+            
+          }
+          
+        }
+        
+      } else { 
+        
+        
+        
+        
+        
+      }
+      
+      
+      
+    }
     
     # check whether the new data suppose to be overwrite or append 
+    ## check dimension
+    ## does the n rows and columns matched?
+    ## anti join between tables 
+    ## if anti join return 0 rows and any of n rows and columns return 0 as well
+    ### skip or next
+    ### else append the table
+    
+    
     
     # save 
     
@@ -235,54 +299,52 @@ sqlite_push <- function(data_list){
 )")
     
     ## Table for service data
-    dbExecute(conn, "CREATE TABLE services (
-             
-             id varchar (100),
-             
-             freq_code char (1) CHECK (freq_code IN ('A')),
-              
-             ref_period_id INT,
-              
-             ref_year INT,
-              
-             ref_month INT,
-              
-             period TEXT,
-              
-             reporter_iso TEXT,
-              
-             repoter_desc TEXT,
-              
-             flow_code TEXT,
-              
-             flow_desc TEXT,
-              
-             partner_iso TEXT,
-              
-             partner2desc TEXT,
-              
-             classification_code char (2),
-              
-             cmd_code varchar (10),
-              
-             cmd_desc varchar (100),
-              
-             aggr_level int,
-              
-             customs_code varchar(15),
-             
-             customs_desc varchar(100),
-              
-             cifvalue bigint,
-              
-             fobvalue bigint,
-              
-             primary_value bigint,
-              
-             PRIMARY KEY (id)
-             
-    )")
+    dbExecute(conn, "CREATE TABLE service (
     
+    id varchar(100),
+    
+    freq_code char(1),
+    
+    ref_period_id INT,
+    
+    ref_year INT,
+    
+    ref_month INT,
+    
+    period TEXT,
+    
+    reporter_iso TEXT,
+    
+    reporter_desc TEXT,
+    
+    flow_code TEXT,
+    
+    flow_desc TEXT,
+    
+    partner_iso TEXT,
+    
+    partner2desc TEXT,
+    
+    classification_code char(2),
+    
+    cmd_code varchar(10),
+    
+    cmd_desc varchar(100),
+    
+    aggr_level int,
+    
+    customs_code varchar(15),
+    
+    customs_desc varchar(100),
+    
+    cifvalue bigint,
+    
+    fobvalue bigint,
+    
+    primary_value bigint,
+    
+    PRIMARY KEY (id)
+)")
     ## Table for Macroeconomic data
     dbExecute(conn, "CREATE TABLE macro (
               
@@ -313,11 +375,14 @@ sqlite_push <- function(data_list){
     )")
     
     # write table into the database
-    # trade goods
+    # goods trade
+    dbWriteTable(conn, name = "goods", trade_new, append = TRUE)
     
-    dbWriteTable(conn, name = "goods", trade, append = TRUE)
+    # service trade
+    dbWriteTable(conn, name = "services", test[[2]], append = TRUE)
     
-     # save
+    # macro trade
+    dbWriteTable(conn, name = "macro", test[[3]], append = TRUE)
     
     
   }
@@ -395,4 +460,4 @@ sqlite_push <- function(data_list){
 
 # function for storing final dataframe into sqlite format
 
-test <- loop_across_countries(country_batches[3], start = "2020", "2023", hs = hs2)
+test <- loop_across_countries(country_batches[1], start = "2020", "2023", hs = hs2)
