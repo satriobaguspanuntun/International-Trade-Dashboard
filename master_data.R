56afbae9af39a3ecdc0048a929551b82b8fe451c
satriobaguspanuntun <satriobaguspanuntun@yahoo.com>
2025-03-13 13:10:51 +0700

    pull_master_data function update.

--- master_data.R
+++ master_data.R
@@ -17,7 +17,7 @@ source("~/international-Trade-Dashboard/data_pull_scripts/pull_macro_data.R")
 source("~/international-Trade-Dashboard/data_pull_scripts/pull_all_data.R")
 options(scipen = 999)
 
-### SET PARAMETERS ###
+#----------------------------- SET PARAMETERS --------------------------------#
 ## set start and end date
 
 # start date
@@ -26,10 +26,10 @@ start_pull_date <- "2015"
 # end date
 end_pull_date <- "2023"
 
-# Connection specs
+## Connection specs
 conn <- dbConnect(SQLite(), "master_db.db")
 
-# puilling relevant concordances
+## puilling relevant concordances
 list_concord <- concordance()
 hs_concord <- list_concord$hs
 serv_concord <- list_concord$serv
@@ -37,32 +37,74 @@ rm(list_concord)
 hs2 <- hs_concord |> filter(aggrLevel == 2) |> select(id)
 hs2 <- hs2$id
 
-# country batches
+## country batches
 country_batches <- country_split()
+
+#-----------------------  PULL TRADE AND MACRO DATA ---------------------------#
 
 # function to iterate over the country batches and stop if data fetch function is called more
 # than 3 times to avoid reaching the maximum amount of calls that API can handle.
 
-pull_master_data <- function(batches, start_date, end_date) {
+pull_master_data <- function(country_batches_to_run, start_date, end_date, hs_type) {
+  
+  current_date <- Sys.time()
+  length_list <- length(country_batches)
   
   # check start and end year format
-  
-  
-  # loop
-  
-  # store to sqlite
-  
-  # done
+  if (year_check(start_date) & year_check(end_date)) {
+    
+    # while loop
+    i <- 1 
+    
+    while (i <= length_list) {
+      
+      # print country batches
+      cli::cli_h1(paste0("Processing country batches: ", i, " out of ", length_list))
+      
+      data_for_sql <- loop_across_countries(batches = country_batches_to_run[i], 
+                                            start = start_date,
+                                            end = end_date,
+                                            hs = hs_type)
+      
+      # store to sqlite
+      sqlite_push(data_for_sql)
+      
+      # if the iterator reach more than 3, stop the loop
+      if (i >= 3) {
+        
+        stop("You have the reach the maximum data pull, please try again tomorrow.")
+        
+        # create a log file 
+        log_data <- data.frame(time = current_date,
+                               date = substr(current_date, 1, 10),
+                               list_step = i,
+                               username = Sys.getenv("USERNAME"))
+        
+        # save it in data folder
+        saveRDS(log_data, "~/international-Trade-Dashboard/data/log_data.rds")
+        
+        cli::cli_h1("Log data has been updated or saved at ~/international-Trade-Dashboard/data folder path")
+      }
+      
+      # print triple dot on the console
+      
+      
+      i <- i + 1
+      
+    }
+    
+    
+  }
   
 }
 
 
-test <- loop_across_countries(country_batches[5], start = "2014", end = "2023", hs = hs2)
+test <- loop_across_countries(country_batches[3], start = "2014", end = "2023", hs = hs2)
 
 sqlite_push(data_list = test)
 
 dbDisconnect(conn)
 
 
-
+pull_master_data(country_batches_to_run = country_batches, start_date = "2020", end_date = "2023", hs_type = hs2)
 
